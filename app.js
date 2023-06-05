3163e180e908969eb4f32c528f04fec80946be37
quantxz <indiedev.core@gmail.com>
2023-06-05 10:32:13 -0300

    chatBackend

---
+++ app.js
@@ -0,0 +1,37 @@
+const WebSocket = require('ws');
+const express = require('express');
+const app = express();
+const path = require('path')
+const serve = require('http').createServer(app)
+
+const port = process.env.PORT || 3000
+
+const server = new WebSocket.Server({ port: port });
+
+let messages = [];
+let connections = [];
+
+server.on("connection", (ws) => {
+    ws.send("Bem-vindo!");
+
+    // Enviar mensagens existentes para o cliente
+    messages.forEach((message) => {
+        ws.send(message);
+    });
+
+    ws.on("message", (message) => {
+        messages.push(message);
+        connections.push(ws);        
+
+        // Enviar a nova mensagem para todos os clientes conectados
+        connections.forEach((client) => {
+            client.send(message);
+        });
+    });
+
+    ws.on("close", () => {
+        console.log("Cliente desconectado.");
+    });
+});
+
+serve.listen(3223)

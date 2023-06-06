67aac0d17a4af0438045fe95c93475a73f73c8d5
quantxz <119700175+quantxz@users.noreply.github.com>
2023-06-06 16:47:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
-const cors = require('cors')
+const cors = require('cors');
 
 const app = express();
 app.use(cors());
@@ -13,8 +13,9 @@ const port = process.env.PORT || 3000;
 let messages = [];
 
 app.get("/", (req, res) => {
-  return res.json(messages)
-})
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.json(messages);
+});
 
 io.on('connection', (socket) => {
   socket.emit('messages', messages);

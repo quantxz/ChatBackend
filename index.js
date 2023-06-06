7c19e1e081b7a201b9950bdfbded8480d07ece2a
quantxz <119700175+quantxz@users.noreply.github.com>
2023-06-06 16:29:14 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,35 @@
 const http = require('http');
-const serverr = http.createServer();
+const express = require('express');
+const socketIO = require('socket.io');
 
-// Importe o servidor WebSocket
-const server = require('./server');
-const port = process.env.PORT || 3000
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
-serverr.on('upgrade', (request, socket, head) => {
-  server.handleUpgrade(request, socket, head, (ws) => {
-    server.emit('connection', ws, request);
+const port = process.env.PORT || 3000;
+
+let messages = [];
+
+io.on('connection', (socket) => {
+  socket.emit('messages', messages);
+
+  // Enviar mensagens existentes para o cliente
+  messages.forEach((message) => {
+    socket.emit('message', message);
+  });
+
+  socket.on('message', (message) => {
+    messages.push(message);
+
+    // Enviar a nova mensagem para todos os clientes conectados
+    io.emit('message', message);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Cliente desconectado.');
   });
 });
 
-serverr.listen(port, () => {
-  return 'Servidor HTTP e WebSocket iniciados na porta 3000.';
+server.listen(port, () => {
+  console.log(`Servidor HTTP e WebSocket iniciados na porta ${port}.`);
 });

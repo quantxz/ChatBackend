f7751cf4348c9158acb88cf7af61d61778fe8d79
quantxz <119700175+quantxz@users.noreply.github.com>
2023-06-06 16:42:02 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
+const cors = require('cors')
 
 const app = express();
+app.use(cors());
 const server = http.createServer(app);
 const io = socketIO(server);
 
